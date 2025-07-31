0ad77312b2a92634dee15ab50e6e0d6e63e29f3f
Jake-Moore <jakealexandermoore@icloud.com>
2025-07-30 18:53:13 -0700

    - fix code rabbit suggestions
    Took 7 minutes

--- core-api/src/main/kotlin/com/jakemoore/datakache/core/connections/changes/ChangeOperationType.kt
+++ core-api/src/main/kotlin/com/jakemoore/datakache/core/connections/changes/ChangeOperationType.kt
@@ -2,6 +2,18 @@ package com.jakemoore.datakache.core.connections.changes
 
 /**
  * Database-agnostic operation types for change streams.
+ *
+ * Document operations:
+ * - INSERT: New document created
+ * - UPDATE: Document modified (partial update)
+ * - REPLACE: Document completely replaced
+ * - DELETE: Document removed
+ *
+ * Administrative operations:
+ * - DROP: Collection dropped
+ * - RENAME: Collection renamed
+ * - DROP_DATABASE: Database dropped
+ * - INVALIDATE: Change stream invalidated
  */
 enum class ChangeOperationType {
     INSERT,
--- core-api/src/main/kotlin/com/jakemoore/datakache/core/connections/changes/ChangeStreamState.kt
+++ core-api/src/main/kotlin/com/jakemoore/datakache/core/connections/changes/ChangeStreamState.kt
@@ -2,6 +2,12 @@ package com.jakemoore.datakache.core.connections.changes
 
 /**
  * Represents the current state of the change stream connection from the database.
+ *
+ * Valid state transitions:
+ * DISCONNECTED -> CONNECTING -> CONNECTED
+ * CONNECTED -> RECONNECTING -> CONNECTED
+ * Any state -> FAILED (on error)
+ * Any state -> SHUTDOWN (on shutdown)
  */
 enum class ChangeStreamState {
     DISCONNECTED,
