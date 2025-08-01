d63b30505e83aef1b46dadb2f74376177b29b6c8
Jake-Moore <jakealexandermoore@icloud.com>
2025-07-31 18:08:12 -0700

    0.0.11 - Update databases shown in console

--- core-api/src/main/kotlin/com/jakemoore/datakache/core/connections/mongo/MongoDatabaseService.kt
+++ core-api/src/main/kotlin/com/jakemoore/datakache/core/connections/mongo/MongoDatabaseService.kt
@@ -145,7 +145,7 @@ internal class MongoDatabaseService : DatabaseService {
             //  This means any admin databases or other network databases are not shown
             val viewable = databaseNames
                 .filter {
-                    it.lowercase().startsWith(DataKache.databaseNamespace)
+                    it.lowercase().startsWith(DataKache.databaseNamespace.lowercase())
                 }
             this.info(viewable.joinToString(", ", prefix = "[", postfix = "]"))
         } catch (timeout: MongoTimeoutException) {
